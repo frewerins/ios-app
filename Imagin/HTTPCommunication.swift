4735e107ccc8c13e627d489c0dba424a1b6f04d5
Knyazeva Anastasia <kniazeva.ad@phystech.edu>
2021-04-04 19:23:32 +0300

    Добавила взаимодействие с сервером

---
+++ Imagin/HTTPCommunication.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+// Наследуем от NSObject, чтобы подчиняться (conform) NSObjectProtocol,
+// потому что URLSessionDownloadDelegate наследует от этого протокола,
+// а раз мы ему подчиняемся, то должны и родительскому протоколу.
+class HTTPCommunication: NSObject {
+    // Свойство completionHandler в классе - это замыкание, которое будет
+    // содержать код обработки полученных с сайта данных и вывода их
+    // в интерфейсе нашего приложения.
+    var completionHandler: ((Data) -> Void)!
+    
+    // retrieveURL(_: completionHandler:) осуществляет загрузку данных
+    // с url во временное хранилище
+    func retrieveURL(_ url: URL, completionHandler: @escaping ((Data) -> Void)) {
+        self.completionHandler = completionHandler
+        let request: URLRequest = URLRequest(url: url)
+        let session: URLSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        let task: URLSessionDownloadTask = session.downloadTask(with: request)
+           // Так как задача всегда создается в остановленном состоянии,
+           // мы запускаем ее.
+           task.resume()
+    }
+    
+    func postURL(_ url: URL, data: Any, completionHandler: @escaping ((Data) -> Void)) {
+        self.completionHandler = completionHandler
+        
+
+        var request: URLRequest = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: data, options: .prettyPrinted)
+        } catch {
+            print("invalid json")
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let session: URLSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        let task: URLSessionDownloadTask = session.downloadTask(with: request)
+        task.resume()
+    }
+}
+
+extension HTTPCommunication: URLSessionDownloadDelegate {
+
+    // Данный метод вызывается после успешной загрузки данных
+    // с сайта во временное хранилище для их последующей обработки.
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        do {
+                // Мы получаем данные на основе сохраненных во временное
+                // хранилище данных. Поскольку данная операция может вызвать
+                // исключение, мы используем try, а саму операцию заключаем
+                // в блок do {} catch {}
+                let data: Data = try Data(contentsOf: location)
+                // Далее мы выполняем completionHandler с полученными данными.
+                // А так как загрузка происходила асинхронно в фоновой очереди,
+                // то для возможности изменения интерфейса, которой работает в
+                // главной очереди, нам нужно выполнить замыкание в главной очереди.
+                DispatchQueue.main.async(execute: {
+                    self.completionHandler(data)
+                })
+            } catch {
+                print("Can't get data from location.")
+            }
+    }
+}
--- Imagin/PhotoController.swift
+++ Imagin/PhotoController.swift
@@ -8,16 +8,9 @@
 import UIKit
 
 class PhotoController: UIViewController {
-    /*func postIMage() {
-        let json : [String: Any]
-        let jsonData =
-        let url = URL(string: "http://google.com") //ссылка на пост запрос на сервер
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = jsonData
-    }*/
-    
+    var photo: UIImage!
     let imagePicker = UIImagePickerController()
+    var nextController = UIViewController()
     
     @IBOutlet weak var nextPage: UIButton!
     
@@ -30,6 +23,10 @@ class PhotoController: UIViewController {
         UIView.transition(with: nextPage, duration: 0.4, options: .transitionCrossDissolve, animations: {() -> Void in
             self.nextPage.isHidden = true
         }, completion: { _ in })
+        self.nextController = storyboard!.instantiateViewController(identifier: "ResultViewController")
+    }
+    @IBAction func sendPhotoAction(_ sender: Any) {
+        sendPhoto()
     }
     @IBOutlet weak var addPhotoButton: UIButton!
     @IBAction func addPhoto(_ sender: Any) {
@@ -51,11 +48,75 @@ class PhotoController: UIViewController {
     }
     @IBOutlet weak var photoFromUser: UIImageView!
     
+    lazy var activityView: UIActivityIndicatorView = {
+        let activityView: UIActivityIndicatorView = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
+        activityView.hidesWhenStopped = true
+        activityView.startAnimating()
+        view.addSubview(activityView)
+        return activityView
+    }()
+    func beforeSeinding() {
+        UIView.transition(with: nextPage, duration: 0.4, options: .transitionCrossDissolve, animations: {() -> Void in
+            self.nextPage.isHidden = true
+        }, completion: { _ in })
+        
+        self.activityView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.activityView.centerXAnchor.constraint(equalTo: self.nextPage.centerXAnchor),
+            self.activityView.centerYAnchor.constraint(equalTo:
+                    self.nextPage.centerYAnchor)
+        ])
+    }
+    
+    func sendPhoto() {
+        beforeSeinding()
+        let http: HTTPCommunication = HTTPCommunication()
+        let url: URL = URL(string: "http://89.223.93.173:5000/process")!
+        let imageData = photo.pngData()
+        let imageBase64 = imageData?.base64EncodedString()
+       // print(imageBase64)
+        let data: [String: Any] = [
+            "image": imageBase64,
+                "metadata": [
+                    "device": "ios"
+                ]
+        ]
+        
+        http.postURL(url, data: data) {
+            [weak self] (data) -> Void in
+                
+            guard let json = String(data: data, encoding: String.Encoding.utf8) else { print("Invalid json")
+                return
+            }
+            print("JSON from server: ", json)
+                    
+            do {
+               /* let jsonObjectAny: Any = try JSONSerialization.jsonObject(with: data, options: [])
+                    
+                guard
+                    let jsonObject = jsonObjectAny as? [String: Any],
+                    let value = jsonObject["value"] as? [String: Any],
+                    //let id = value["id"] as? Int,
+                    let joke = value["joke"] as? String else {
+                        return
+                    }*/
+                //self?.nextPage.setTitle(joke, for: .normal)
+                self?.activityView.stopAnimating()
+                UIView.transition(with: self!.nextPage, duration: 0.4, options: .transitionCrossDissolve, animations: {() -> Void in
+                    self?.nextPage.isHidden = false
+                }, completion: { _ in })
+                self!.navigationController!.pushViewController(self!.nextController, animated: true)
+            } catch {
+                print("Can't serialize data.")
+            }
+        }
+    }
 }
+
 extension PhotoController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedPhoto = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            // self.photo = pickedPhoto сделаю вьюшку image и кнопки типо повторить
+            photo = pickedPhoto
             photoFromUser.image = pickedPhoto
             print("get photo!!")
             UIView.transition(with: photoFromUser, duration: 0.4, options: .transitionCrossDissolve, animations: {() -> Void in
