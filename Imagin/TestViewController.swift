4e9741dbc1476df1983689835422ef1bccd75af1
Knyazeva Anastasia <kniazeva.ad@phystech.edu>
2021-04-18 11:20:29 +0300

    IM-67 Поправила скролл

--- Imagin/TestViewController.swift
+++ Imagin/TestViewController.swift
@@ -34,52 +34,12 @@ class TestViewController: UIViewController, UIScrollViewDelegate {
         }, completion: { _ in })
     }
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if (scrollView.contentOffset.x.truncatingRemainder(dividingBy: 5) == 0) {
-            scrollView.contentOffset.x = CGFloat(scrollView.contentOffset.x - 2)
-        }
-       // if (abs(scrollView.contentOffset.x - lastOffset) > 20) {
-            
-            /*let width = Float(scrollView.contentSize.width)
-            let screenWidth = Float(UIScreen.main.bounds.width)
-            var target = 0
-            if (scrollView.contentOffset.x > lastOffset) {
-                currentPage = min(currentPage + 1, 2);
-            } else {
-                currentPage = max(currentPage - 1, 0);
-            }
-            self.pageControl.currentPage = currentPage
-            if (currentPage == 1) {
-                target = Int(width / 2 - screenWidth / 2)
-            }
-            if (currentPage == 2) {
-                target = Int(width / 2 + screenWidth / 2)
-                UIView.transition(with: label, duration: 0.8, options: .transitionCrossDissolve, animations: {() -> Void in
-                    self.startButton.isHidden = false
-                }, completion: { _ in })
-            }
-            
-            self.lastOffset = CGFloat(target)
-        
-            let bottomOffset = CGPoint(x: self.lastOffset + 50, y: 0)
-            
-            //scrollView.isScrollEnabled = false
-            
-            UIView.animate(withDuration: 5, animations: {
-                scrollView.setContentOffset(bottomOffset, animated: true)
-            })*/
-            //scrollView.isScrollEnabled = true
-       // }
-    }
     
     
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         let width = Float(scrollView.contentSize.width)
         let screenWidth = Float(UIScreen.main.bounds.width)
         print(scrollView.contentOffset.x)
-        if (abs(scrollView.contentOffset.x - lastOffset) < 10) {
-            return
-        }
         if (scrollView.contentOffset.x > lastOffset) {
             currentPage = min(currentPage + 1, 2);
         } else {
@@ -93,8 +53,8 @@ class TestViewController: UIViewController, UIScrollViewDelegate {
             targetContentOffset.pointee.x = CGFloat(width / 2 - screenWidth / 2)
         }
         if (currentPage == 2) {
-            targetContentOffset.pointee.x = CGFloat(width / 2 + screenWidth / 2)
-            UIView.transition(with: label, duration: 0.8, options: .transitionCrossDissolve, animations: {() -> Void in
+            targetContentOffset.pointee.x = CGFloat(width - screenWidth)
+            UIView.transition(with: label, duration: 0.2, options: .transitionCrossDissolve, animations: {() -> Void in
                 self.startButton.isHidden = false
             }, completion: { _ in })
         }
@@ -103,14 +63,13 @@ class TestViewController: UIViewController, UIScrollViewDelegate {
         
         let bottomOffset = CGPoint(x: self.lastOffset, y: 0)
         
-        UIView.animate(withDuration: 1, animations: {
+        UIView.animate(withDuration: 0.3, animations: {
             scrollView.setContentOffset(bottomOffset, animated: true)
         })
         
         UIView.animate(withDuration: 1, animations: {
             self.label.alpha = 0
         })
-        
         self.label.text = self.labels[self.currentPage]
         UIView.animate(withDuration: 1, animations: {
             self.label.alpha = 1
@@ -120,14 +79,9 @@ class TestViewController: UIViewController, UIScrollViewDelegate {
         UIView.animate(withDuration: 1, animations: {
             self.descr.alpha = 0
         })
-        
         self.descr.text = self.descrs[self.currentPage]
         UIView.animate(withDuration: 1, animations: {
             self.descr.alpha = 1
         })
-        
-       /*UIView.transition(with: label, duration: 0.6, options: .transitionCrossDissolve, animations: {() -> Void in
-            self.label.text = self.labels[self.currentPage]
-        }, completion: { _ in })*/
       }
 }
