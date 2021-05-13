d6aef14fd97c6be11a4275efee51a1a087907c7b
Knyazeva Anastasia <kniazeva.ad@phystech.edu>
2021-05-13 10:55:18 +0300

    IM-85

--- Imagin/SeasonCollectionViewCell.swift
+++ Imagin/SeasonCollectionViewCell.swift
@@ -7,10 +7,6 @@
 
 import UIKit
 
-let colorss = [UIColor(red: 222/255, green: 19/255, blue: 84/255, alpha: 1),
-    UIColor(red: 226/255, green: 106/255, blue: 71/255, alpha: 1),
-    UIColor(red: 224/255, green: 78/255, blue: 20/255, alpha: 1)]
-
 class SeasonCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var label: UILabel!
